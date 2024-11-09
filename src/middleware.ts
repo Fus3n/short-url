a8224831e9ceb1cd898a3876a11dd5db98ea01a1
Fus3n <fus3ngames@gmail.com>
2024-11-10 03:45:17 +0600

    debug #2

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,33 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // Add a new header x-current-path which passes the path to downstream components
-  const headers = new Headers(request.headers);
-  headers.set("x-current-path", request.nextUrl.pathname);
-  headers.set("x-user-country", request.geo?.country || "Unknown");
-  return NextResponse.next({ headers });
+  const response = NextResponse.next();
+  const sessionCookie = request.cookies.get('session_id');
+  if (sessionCookie) {
+    const cookieAttributes = sessionCookie.toString().split(';');
+    const maxAgeStr = cookieAttributes.find(attr => attr.trim().startsWith('max-age='));
+    
+    if (maxAgeStr) {
+      const maxAge = parseInt(maxAgeStr.split('=')[1]);
+      if (maxAge > 0) {
+        response.cookies.set({
+          name: 'session_id',
+          value: sessionCookie.value,
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: 'lax',
+          path: '/',
+          maxAge: maxAge // Use the original maxAge
+        });
+      }
+    }
+  }
+
+
+  // const headers = new Headers(request.headers);
+  response.headers.set("x-current-path", request.nextUrl.pathname);
+  response.headers.set("x-user-country", request.geo?.country || "Unknown");
+  return response;
 }
 
 export const config = {
