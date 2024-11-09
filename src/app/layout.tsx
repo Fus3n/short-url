ae61eb7458541cf5c6e214ad719d2fbc2c0c64d1
Fus3n <fus3ngames@gmail.com>
2024-11-10 01:00:39 +0600

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 
 import { Toaster } from "@/components/ui/toaster"
 
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ShortLinks",
   description: "Shorten your links and share them with your audience.",
+  icons: {
+    icon: "/logo-svg.svg",
+  }
 };
 
 export default function RootLayout({
